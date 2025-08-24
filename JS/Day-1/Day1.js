bb8b8ae9cde194f8c72f378c20c6196f9fe062e4
Ahmed Mohameden <76133636+AhmedMohameden@users.noreply.github.com>
2025-08-24 04:08:02 +0300

    Day1.js

--- JS/Day-1/Day1.js
+++ JS/Day-1/Day1.js
@@ -10,7 +10,7 @@ function fun(){
 
     document.getElementById("www").innerHTML+= pro +"<br>";
     console.log(`${pro}`);
-    
+     window.alert(pro);
      name=document.getElementById("name").value="";
      price=document.getElementById("price").value="";
      des=document.getElementById("des").value="";
